28f85406637ebe21095b601c8faf48eb4bec7334
user <user@LAPTOP-9FLV1FQJ>
2020-12-08 19:01:55 +0900

    initial commit

---
+++ src/Solution.java
@@ -0,0 +1,17 @@
+
+public class Solution {
+	int gS =10;
+	static int sd = 7;
+
+	public void Test(int value) {
+	int lS = 30;
+	}
+	
+	
+	public static void main(String[] args) {
+		Solution d = new Solution();
+		d.gS = 5;
+		System.out.println(d);
+		System.out.println(sd);
+
+	}}
